fe73cd7e68e84e14b3f36abba341d2d1342f3a11
laluahmadcom <108070884+laluahmadcom@users.noreply.github.com>
2022-08-15 10:42:49 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,142 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+include 'components/like_post.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home page</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<section class="home-grid">
+
+   <div class="box-container">
+
+      <div class="box">
+         <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1s-g8LzI46_srScG8i9ixLczcVPTUlhg&ehbc=2E312F" width="640" height="480"></iframe>
+      </div>
+
+      <div class="box">
+         <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1s-g8LzI46_srScG8i9ixLczcVPTUlhg&ehbc=2E312F" width="640" height="480"></iframe>
+      </div>
+
+      <div class="box">
+         <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1s-g8LzI46_srScG8i9ixLczcVPTUlhg&ehbc=2E312F" width="640" height="480"></iframe>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="posts-container">
+
+   <h1 class="heading">latest posts</h1>
+
+   <div class="box-container">
+
+      <?php
+         $select_posts = $conn->prepare("SELECT * FROM `posts` WHERE status = ? LIMIT 6 ");
+         $select_posts->execute(['active']);
+         if($select_posts->rowCount() > 0){
+            while($fetch_posts = $select_posts->fetch(PDO::FETCH_ASSOC)){
+               
+               $post_id = $fetch_posts['id'];
+
+               $count_post_comments = $conn->prepare("SELECT * FROM `comments` WHERE post_id = ?");
+               $count_post_comments->execute([$post_id]);
+               $total_post_comments = $count_post_comments->rowCount(); 
+
+               $count_post_likes = $conn->prepare("SELECT * FROM `likes` WHERE post_id = ?");
+               $count_post_likes->execute([$post_id]);
+               $total_post_likes = $count_post_likes->rowCount();
+
+               $confirm_likes = $conn->prepare("SELECT * FROM `likes` WHERE user_id = ? AND post_id = ?");
+               $confirm_likes->execute([$user_id, $post_id]);
+      ?>
+      <form class="box" method="post">
+         <input type="hidden" name="post_id" value="<?= $post_id; ?>">
+         <input type="hidden" name="admin_id" value="<?= $fetch_posts['admin_id']; ?>">
+         <div class="post-admin">
+            <i class="fas fa-user"></i>
+            <div>
+               <a href="author_posts.php?author=<?= $fetch_posts['name']; ?>"><?= $fetch_posts['name']; ?></a>
+               <div><?= $fetch_posts['date']; ?></div>
+            </div>
+         </div>
+         
+         <?php
+            if($fetch_posts['image'] != ''){  
+         ?>
+         <img src="uploaded_img/<?= $fetch_posts['image']; ?>" class="post-image" alt="">
+         <?php
+         }
+         ?>
+         <div class="post-title"><?= $fetch_posts['title']; ?></div>
+         <div class="post-content content-150"><?= $fetch_posts['content']; ?></div>
+         <a href="view_post.php?post_id=<?= $post_id; ?>" class="inline-btn">read more</a>
+         
+         </div>
+      
+      </form>
+      <?php
+         }
+      }else{
+         echo '<p class="empty">no posts added yet!</p>';
+      }
+      ?>
+   </div>
+
+   <div class="more-btn" style="text-align: center; margin-top:1rem;">
+      <a href="posts.php" class="inline-btn">view all posts</a>
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
